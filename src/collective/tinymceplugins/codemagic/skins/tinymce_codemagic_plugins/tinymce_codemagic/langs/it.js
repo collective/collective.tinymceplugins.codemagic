6cf8559afe54ac3fbae7fba9c7bbaf632cd58ebb
Giorgio Borelli <casaborelli@gmail.com>
2012-04-13 16:28:41 +0200

    added italian translation

---
+++ src/collective/tinymceplugins/codemagic/skins/tinymce_codemagic_plugins/tinymce_codemagic/langs/it.js
@@ -0,0 +1,3 @@
+tinyMCE.addI18n('it.codemagic',{
+    editor_button: "Modifica codice sorgente"
+});
---
+++ src/collective/tinymceplugins/codemagic/skins/tinymce_codemagic_plugins/tinymce_codemagic/langs/it_dlg.js
@@ -0,0 +1,14 @@
+tinyMCE.addI18n('it.codemagic_dlg',{
+    code_title: "CodeMagic - editor sorgente",
+    code_label: "Modifica codice",
+    toggle_highlighting: "Evidenzia codice",
+    toggle_autocompletion: "Autocompletamento",
+    search: "Cerca",
+    replace: "Sostituisci",
+    undo: "Annulla",
+    redo: "Ripeti",
+    search_replace: "Cerca e sostituisci",
+    reintendt: "Formatta il codice HTML",
+    nothing_found: "Nessuna corrispondenza trovata.",
+    nothing_to_replace: "Nessuna corrispondenza da sostituire."
+});
